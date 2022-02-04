ea89e5ccc47433689d715a10ea9972a1ec6ceb12
hasnatulalam <hasnatualam056@gmail.com>
2022-02-04 21:41:26 +0600

    final

--- src/App.js
+++ src/App.js
@@ -1,88 +1,44 @@
-<<<<<<< HEAD
 import { useEffect } from 'react';
 import Header from './components/Header';
 import About from './components/About';
 import Skills from './components/Skills';
 import Portfolio from './components/Portfolio';
-import Service from './components/Service';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import TopscrollButton from './components/TopscrollButton';
-import Snowfall from 'react-snowfall'
-=======
-import React, { createContext, useState } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import Service from './components/Service/Service';
+import Footer from './components/Footer/Footer'
 
-
-import Blogs from './components/Blogs/Blogs';
-import Home from './components/Home/Home';
-
-/* import Projects from './components/Projects/Projects'; */
->>>>>>> origin
+import TopscrollButton from './components/TopscrollButton/TopscrollButton';
 import AOS from 'aos';
-
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+import 'aos/dist/aos.css';
+import 'font-awesome/css/font-awesome.min.css';
+import './styles.css';
 import Contact from './components/Contact/Contact';
-import About from './components/About/About';
-
-export const activeContext = createContext();
-
-library.add(fab, faCheckSquare, faCoffee)
 
 
 function App() {
-
-  AOS.init();
-
-  // sessionStorage.setItem("active", "home");
-  const [active, setActive] = useState('home');
-
+  useEffect(() => {
+    AOS.init(
+      {
+        duration: 2000,
+        delay: 10,
+        startEvent: 'DOMContentLoaded',
+        initClassName: 'aos-init',
+        once: true
+      }
+    );
+  }, [])
   return (
-<<<<<<< HEAD
-    
+
     <div className="App">
-      <Header/>
-      <Snowfall />
-      <About/>
-      <Skills/>
-      <Service/>
-      <Portfolio/> 
-    
-      <Contact/>
-      <Footer/>
-      <TopscrollButton/>
+      <Header />
+      <About />
+      <Skills />
+      <Service />
+      <Portfolio />
+      <Contact></Contact>
+      <Footer></Footer>
+      <TopscrollButton />
     </div>
-    
-=======
-    <activeContext.Provider value={[active, setActive]}>
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/blogs">
-            <Blogs />
-          </Route>
-         {/*  <Route path="/portfolio">
-            <Projects />
-          </Route> */}
-          <Route path="/contact">
-            <Contact />
-          </Route>
-         
-        </Switch>
-      </Router>
-    </activeContext.Provider>
->>>>>>> origin
+
   );
 }
 
